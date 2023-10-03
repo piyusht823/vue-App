b9c9c3ce7ce998be853f71687a5b0353ecca0e33
Piyush <piyusht823@gmail.com>
2023-10-03 16:40:41 +0530

    Options API vs Compostion API Code

---
+++ src/components/OptionsvsComposition.vue
@@ -0,0 +1,140 @@
+<!-- <script>
+//Options API
+export default {
+  data() {
+    return {
+      username: 'Piyush',
+      isModalShown: false
+    }
+  }
+  ,
+  methods: {
+    updateUsername(newUsername) {
+      this.username = newUsername
+    },
+    showModal() {
+      this.isModalShown = true
+    },
+    mounted() {
+      this.updateUsername('Piyush@LT')
+      this.showModal()
+    }
+  }
+}
+
+</script>
+
+<template>
+    <div>
+      <p>{{ username }}</p>
+      <button @click="updateUsername('Piyush@LTdI')">Update Username</button>
+      <button @click="showModal">Show Modal</button>
+    </div>
+</template> -->
+  
+
+
+
+
+
+
+
+
+<!-- Compositions API -->
+<script setup>
+import { onMounted, ref } from 'vue';
+
+
+      const username= ref('Piyush')
+      const isModalShown= ref(false)
+
+    function updateUsername(newUsername) {
+      username.value = newUsername
+    }
+    function showModal() {
+      isModalShown.value = false
+    }
+    
+    onMounted(() => {
+        updateUsername('Tiwari');
+        showModal();
+    })
+
+</script>
+
+<template>
+    <div>
+      <p>{{ username }}</p>
+      <p>{{ isModalShown }}</p>
+      <button @click="updateUsername('Pratyush Tiwari')">Update Username</button>
+    </div>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- OR -->
+
+
+<!-- Compostion API by AI-->
+<!-- <template>
+  <div>
+    <p>{{ username }}</p>
+    <button @click="updateUsername('Tiwari')">Update Username</button>
+  </div>
+</template> 
+
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    // Define reactive data using ref
+    const username = ref('Piyush');
+    const isModalShown = ref(false);
+
+    // Define methods
+    const updateUsername = (newUsername) => {
+      username.value = newUsername;
+    };
+
+    const showModal = () => {
+      isModalShown.value = true;
+    };
+
+    // Component lifecycle hook: mounted
+    onMounted(() => {
+      updateUsername('Piyush@LTI');
+      showModal();
+    });
+
+    // Return data and methods for use in the template
+    return {
+      username,
+      isModalShown,
+      updateUsername,
+      showModal,
+    };
+  },
+};
+</script>  -->
