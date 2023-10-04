eb528b2de4e6f4adae1700c3b510dddc00ec6daf
Piyush <piyusht823@gmail.com>
2023-10-04 13:49:46 +0530

    variable way of adding attribute

--- src/components/vbind.vue
+++ src/components/vbind.vue
@@ -6,12 +6,13 @@ import {ref} from 'vue';
     const textcolor = ref('textstyle')
     const content = ref('<b>Mindtree</b>')
     const status = ref(true)
-    // const attr1 = ref('id')
-    // const attr2 = ref('class')
+    const attr1 = ref('id')
+    const attr2 = ref('class')
 </script>
 
 <template>
-    <div :id="empid" :class="textcolor" v-html="content" v-show="status">
+    <!-- <div :id="empid" :class="textcolor" v-html="content" v-show="status"> -->
+    <div :[attr1]="empid" :[attr2]="textcolor" v-html="content" v-show="status">
 
     </div>
 </template>
